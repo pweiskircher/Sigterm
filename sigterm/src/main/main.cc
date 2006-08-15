38e4eafc7125ae212115524ded56aa1f8d2da828
Christian Hofstaedtler <ch@zeha.at>
2006-08-15 18:00:27 +0000

    build fixes for windows (including a broken WinMain so we are more gui-like)

--- sigterm/src/main/main.cc
+++ sigterm/src/main/main.cc
@@ -3,7 +3,17 @@
 #include <QApplication>
 #include "MainWindow.h"
 
+#ifdef WIN32
+#include <windows.h>
+#endif
+
+#ifdef WIN32
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow ) {
+	int argc = 0;
+	char **argv = NULL;
+#else
 int main(int argc, char **argv) {
+#endif
     QApplication app(argc, argv);
     MainWindow m;
     m.show();
