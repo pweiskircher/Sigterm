c53eea6262a5a0a33b47e56acba46e4f634b8e29
Patrik Weiskircher <github@weiskircher.name>
2006-08-18 21:02:07 +0000

    indenting

--- sigterm/src/audio/AudioDecoder.cc
+++ sigterm/src/audio/AudioDecoder.cc
@@ -3,94 +3,94 @@
 #include "AudioBuffer.h"
 
 AudioDecoder::AudioDecoder(AudioFile *inAudioFile, AudioManager *inAudioManager) : mConverter(inAudioManager) {
-    mBuiltCVT = false;
-    mAudioManager = inAudioManager;
-    mAudioFile = inAudioFile;
-    mOpened = false;
+	mBuiltCVT = false;
+	mAudioManager = inAudioManager;
+	mAudioFile = inAudioFile;
+	mOpened = false;
 }
 
-AudioDecoder::~AudioDecoder() {
-    if (opened())
-	close();
-}
+	AudioDecoder::~AudioDecoder() {
+		if (opened())
+			close();
+	}
 
 bool AudioDecoder::open() {
-    if (opened()) {
-	qDebug("File already opened.");
-	return false;
-    }
+	if (opened()) {
+		qDebug("File already opened.");
+		return false;
+	}
 
-    bool r = openFile();
-    if (r)
-	setOpened(true);
-    return r;
+	bool r = openFile();
+	if (r)
+		setOpened(true);
+	return r;
 }
 
 bool AudioDecoder::close() {
-    if (!opened()) {
-	qDebug("File not open.");
-	return false;
-    }
+	if (!opened()) {
+		qDebug("File not open.");
+		return false;
+	}
 
-    bool r = closeFile();
-    if (r)
-	setOpened(false);
-    return r;
+	bool r = closeFile();
+	if (r)
+		setOpened(false);
+	return r;
 }
 
 bool AudioDecoder::opened() {
-    return mOpened;
+	return mOpened;
 }
 
 AudioFormat &AudioDecoder::audioFormat() {
-    return mAudioFormat;
+	return mAudioFormat;
 }
 
 AudioDecoder::DecodingStatus AudioDecoder::getAudioChunk(AudioBuffer *inOutAudioBuffer) {
-    if (!opened()) {
-	if (!open())
-	    return eStop;
-    }
+	if (!opened()) {
+		if (!open())
+			return eStop;
+	}
 
-    if (!mBuiltCVT) {
-	mConverter.setSourceFormat(&audioFormat());
-	mBuiltCVT = true;
-    }
+	if (!mBuiltCVT) {
+		mConverter.setSourceFormat(&audioFormat());
+		mBuiltCVT = true;
+	}
 
-    AudioDecoder::DecodingStatus status;
-    status = getDecodedChunk(inOutAudioBuffer);
-    if (status == eStop) {
-	close();
-    }
+	AudioDecoder::DecodingStatus status;
+	status = getDecodedChunk(inOutAudioBuffer);
+	if (status == eStop) {
+		close();
+	}
 
-    if (inOutAudioBuffer->state() != AudioBuffer::eGotDecodedChunk) {
+	if (inOutAudioBuffer->state() != AudioBuffer::eGotDecodedChunk) {
+		return status;
+	}
+
+	if (!mConverter.convert(inOutAudioBuffer)) {
+		qDebug("Could not convert audio!");
+		close();
+		return eStop;
+	}
+
+	// sanity check
+	if (inOutAudioBuffer->state() != AudioBuffer::eGotConvertedChunk) {
+		qDebug("AudioBuffer state wrong.");
+		close();
+		return eStop;
+	}
+
 	return status;
-    }
-
-    if (!mConverter.convert(inOutAudioBuffer)) {
-	qDebug("Could not convert audio!");
-	close();
-	return eStop;
-    }
-
-    // sanity check
-    if (inOutAudioBuffer->state() != AudioBuffer::eGotConvertedChunk) {
-	qDebug("AudioBuffer state wrong.");
-	close();
-	return eStop;
-    }
-
-    return status;
 }
 
 AudioManager *AudioDecoder::audioManager() {
-    return mAudioManager;
+	return mAudioManager;
 }
 
 AudioFile *AudioDecoder::audioFile() {
-    return mAudioFile;
+	return mAudioFile;
 }
 
 void AudioDecoder::setOpened(bool inValue) {
-    mOpened = inValue;
+	mOpened = inValue;
 }
