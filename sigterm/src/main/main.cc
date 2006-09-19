3a6bf1b6862c3c99b93e1d97a71a95f0f33a6b69
Patrik Weiskircher <github@weiskircher.name>
2006-09-19 19:41:40 +0000

    set plastique as default style

--- sigterm/src/main/main.cc
+++ sigterm/src/main/main.cc
@@ -2,9 +2,11 @@
 
 #include <QApplication>
 #include "MainWindow.h"
+#include <QPlastiqueStyle>
 
 int main(int argc, char **argv) {
 	QApplication app(argc, argv);
+	QApplication::setStyle(new QPlastiqueStyle);
 	MainWindow m;
 	m.show();
 	return app.exec();
